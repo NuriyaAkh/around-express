3e4f80401d378452a6a25c83bcfa099f8fc416e4
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-06-27 22:24:56 -0400

    prettier

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,11 +3,14 @@ const { getDataFromFile } = require("../helpers/files");
 
 const cardsDataPath = path.join(__dirname, "..", "data", "cards.json");
 
-const getCards = (req, res) =>{
+const getCards = (req, res) => {
   return getDataFromFile(cardsDataPath)
-  .then((cards) => res.status(200).send(cards))
-  .catch((err) => res.status(500).send({"message":`"An error has occurred on the
-  server ${err}`}));
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) =>
+      res
+        .status(500)
+        .send({ message: `"An error has occurred on the server ${err}` })
+    );
 };
 
-module.exports = {getCards};
+module.exports = { getCards };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
-const router = require("express").Router(); 
+const router = require("express").Router();
 const { getUsers, getUsersById } = require("../controllers/user");
 
 router.get("/users", getUsers);
 router.get("/users/:id", getUsersById);
-
 
 module.exports = router;
